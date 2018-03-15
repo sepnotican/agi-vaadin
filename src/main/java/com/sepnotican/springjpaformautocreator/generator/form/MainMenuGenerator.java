4f0b4ee424171664ce069e42dd10f5c376eb88dd
Мурадов Музафар Алиевич <m.muradov@butik.ru>
2018-03-15 16:54:12 +0300

    +tab sortring

--- src/main/java/com/sepnotican/springjpaformautocreator/generator/form/MainMenuGenerator.java
+++ src/main/java/com/sepnotican/springjpaformautocreator/generator/form/MainMenuGenerator.java
@@ -8,6 +8,7 @@ import org.reflections.Reflections;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Comparator;
 import java.util.Set;
 
 public class MainMenuGenerator {
@@ -30,18 +31,24 @@ public class MainMenuGenerator {
             Reflections reflections = new Reflections(prefix);
             Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(AgiUI.class);
 
-            for (Class<?> aClass : annotated) {
+            annotated.stream().sorted(new Comparator<Class<?>>() {
+                @Override
+                public int compare(Class<?> o1, Class<?> o2) {
+                    String name1 = o1.getAnnotation(AgiUI.class).listCaption();
+                    String name2 = o2.getAnnotation(AgiUI.class).listCaption();
+                    return name1.compareTo(name2);
+                }
+            }).forEach(aClass -> {
                 AgiUI agiUI = aClass.getAnnotation(AgiUI.class);
                 Class repositoryClass = agiUI.repo();
                 JpaRepository repository = (JpaRepository) context.getBean(repositoryClass);
 
                 MenuBar.MenuItem item = menuBar.addItem(agiUI.listCaption(), agiUI.icon(),
                         event -> listFormHandler.showAbstractListForm(aClass, repository));
-            }
-
+                layoutForMenu.addComponent(menuBar);
+            });
         }
-
-        layoutForMenu.addComponent(menuBar);
     }
-
 }
+
+
