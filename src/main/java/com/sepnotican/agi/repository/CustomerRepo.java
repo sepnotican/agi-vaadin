eecb0898b420d289110297aea80a6dab61fb312d
sepnotican <sepnotican@gmail.com>
2018-05-14 23:26:06 +0300

    -redundant code

--- src/main/java/com/sepnotican/agi/repository/CustomerRepo.java
+++ src/main/java/com/sepnotican/agi/repository/CustomerRepo.java
@@ -4,13 +4,7 @@ import com.sepnotican.agi.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
-    Customer save(Customer customer);
 
-    Optional<Customer> findById(Long id);
-
-    Customer findFirstByName(String name);
 }
