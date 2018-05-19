6b545b0386d96a72c79945520ec8f12642aa745f
sepnotican <sepnotican@gmail.com>
2018-05-19 15:01:00 +0300

    generic repository

--- src/main/java/com/sepnotican/agi/core/utils/GenericDaoFactory.java
+++ src/main/java/com/sepnotican/agi/core/utils/GenericDaoFactory.java
@@ -1,4 +1,12 @@
 package com.sepnotican.agi.core.utils;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class GenericDaoFactory {
+
+    public GenericDao getGenericDaoForClass() {
+
+    }
+
 }
